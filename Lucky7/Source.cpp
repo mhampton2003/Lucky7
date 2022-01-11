19387921ae13949746ea087855ca63f169878e17
Maya Hampton <mhampton2003@gmail.com>
2022-01-11 09:37:11 -0700

    added function headers

---
+++ Lucky7/Source.cpp
@@ -0,0 +1,50 @@
+//Maya Hampton
+//Lucky 7
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+void f1();
+void f2();
+void f3();
+void f4();
+void f5();
+void f6();
+void f7();
+
+int main() {
+
+
+
+	return 0;
+}
+
+void f1()
+{
+}
+
+void f2()
+{
+}
+
+void f3()
+{
+}
+
+void f4()
+{
+}
+
+void f5()
+{
+}
+
+void f6()
+{
+}
+
+void f7()
+{
+}
