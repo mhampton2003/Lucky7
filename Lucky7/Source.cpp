a5819236fac9054231ae2a0aee6b955db2071733
Maya Hampton <mhampton2003@gmail.com>
2022-01-11 09:52:20 -0700

    added f1()

--- Lucky7/Source.cpp
+++ Lucky7/Source.cpp
@@ -16,13 +16,33 @@ void f7();
 
 int main() {
 
-
+	f1();
 
 	return 0;
 }
 
+//reads in 5 integers
+//prints the largest and the smallest of the group
 void f1()
 {
+	int curr, large, small;
+
+	cout << "Enter 5 integers: ";
+	cin >> curr;
+
+	//first entry is both small and large
+	small = large = curr;
+
+	for (int i = 1; i <= 4; i++) {
+		cin >> curr;
+
+		if (curr < small)
+			small = curr;
+		else if (curr > large)
+			large = curr;
+	}
+
+	cout << "largest = " << large << " smallest = " << small << endl;
 }
 
 void f2()
