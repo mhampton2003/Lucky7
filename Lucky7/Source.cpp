4e93b31fa3f2032dc56ea67a36d1bd9dabc14d3b
Maya Hampton <mhampton2003@gmail.com>
2022-01-11 10:06:40 -0700

    added f2()

--- Lucky7/Source.cpp
+++ Lucky7/Source.cpp
@@ -16,7 +16,8 @@ void f7();
 
 int main() {
 
-	f1();
+	//f1();
+	//f2();
 
 	return 0;
 }
@@ -45,8 +46,14 @@ void f1()
 	cout << "largest = " << large << " smallest = " << small << endl;
 }
 
+//calculates and prints the sum of the first 50 
+//positive integers that are multiples of 7. 
 void f2()
 {
+	int sum = 0;
+	for (int i = 1; i <= 50; i++)
+		sum += i * 7;
+	cout << "sum = " << sum << endl;
 }
 
 void f3()
